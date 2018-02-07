cc599806f1d0f097c65d38ebba65f1b3200ce495
svc-student <svc-student@student.directory.jccc>
2018-02-06 18:52:06 -0600

    updating Updeter.bat adn reverter.bat

--- Updater.bat
+++ Updater.bat
@@ -1,3 +1,8 @@
 @echo off 
 
 REM pulls from repository and merges with the local one being worked on
+
+REM %1 will be the branch being merged into master
+
+git checkout master
+git merge %1
--- reverter.bat
+++ reverter.bat
@@ -1,3 +1,7 @@
 @echo off 
 
 REM reverts changes made
+
+REM merges current branch to the master branch
+
+git merge -Xtheirs master
