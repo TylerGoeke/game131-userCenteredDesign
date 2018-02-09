00b661355239222a9f70e09216701b1fda1cfca0
TylerGoeke <minifigfencing@hotmail.com>
2018-02-08 18:24:26 -0600

    committing for update

--- Updater.bat
+++ Updater.bat
@@ -4,4 +4,6 @@ REM pulls from repository and merges with the local one being worked on
 
 REM pulls origin to update local repository from original repository
 
+git add --all
+git commit -m "committing for update"
 git pull origin
--- innitialize.bat
+++ innitialize.bat
@@ -1,7 +1,8 @@
 @echo off
 
-REM %1 should be html for repository. %2 should be username. %3 should be email
+REM %1 should be html for repository. %2 should be the desired branch. %3 should be username. %4 should be email
 
 git clone %1
-git config user.name %2
-git config user.email %3
+git branch %2
+git config user.name %3
+git config user.email %4
