9eb3aab076693c8fec1ba6de6487d5a808366699
TylerGoeke <minifigfencing@hotmail.com>
2018-02-08 19:04:45 -0600

    updating committer

--- innitialize.bat
+++ innitialize.bat
@@ -5,5 +5,6 @@ REM %1 should be the desired branch. %2 should be username. %3 should be email
 
 git branch %1
 git checkout %1
+git push --set-upstream origin %1
 git config user.name %2
 git config user.email %3
