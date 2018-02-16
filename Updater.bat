9d4b9ded1013211184dde703995e8e97c1b8419c
svc-student <svc-student@student.directory.jccc>
2018-02-15 18:17:30 -0600

    committing for update

--- Updater.bat
+++ Updater.bat
@@ -7,3 +7,18 @@ REM pulls origin to update local repository from original repository
 git add --all
 git commit -m "committing for update"
 git pull origin
+
+IF %1=="/?"(
+	goto help ) 
+	else (
+	goto run )
+:run
+git add --all
+git commit -m "committing for update"
+git pull origin	
+goto exit
+:help
+echo helpCheck
+
+:exit
+exit /b
