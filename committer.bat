f36e324cdfff8f1fd17c5e178b42252e15611780
TylerGoeke <minifigfencing@hotmail.com>
2018-02-15 18:41:50 -0600

    fixing committer.bat

--- committer.bat
+++ committer.bat
@@ -2,22 +2,6 @@
 
 REM moves to master, adds all files to next commit, commits the files with a message, and pushes them
 
-<<<<<<< HEAD
-if %1=="--help" (
-	goto helping
-) ELSE (
-
-	git add --all
-	git commit -m "%~1"
-	git push
-	goto break
-)
-:helping
-echo This file commits all changes made in the current branch and pushes them to the repository
-echo An input consisting of a commit message in quotation marks (" ") is required
-
-:break
-=======
 set checkOne=%1
 
 IF %checkOne%==help GOTO help
@@ -32,5 +16,4 @@ echo This tool commits all files and then pushes them to the repository using th
 echo the message is required
 
 :exit
->>>>>>> master
 exit /b
